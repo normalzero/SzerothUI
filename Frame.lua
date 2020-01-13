2288c62cebc0a306fe2ae0ff064e1dc6086436f7
咸蛋 <saltyzero@foxmail.com>
2020-01-13 18:22:51 +0800

    Update Frame.lua

--- Frame.lua
+++ Frame.lua
@@ -1,9 +1,9 @@
 -- function: controller callback
 -- author: saltyzero@foxmail.com
 
-local ttt = CreateFrame("Frame")
-ttt:RegisterEvent("CHAT_MSG_ADDON")
-ttt:SetScript("OnEvent", function(self, event, prefix, msg, type, sender)
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("CHAT_MSG_ADDON")
+frame:SetScript("OnEvent", function(self, event, prefix, msg, type, sender)
 	Data:test()
 
     local item = ItemRandomData:new({}, {123456, 0, 3, 1.15, {1.1}, {1.2}})
